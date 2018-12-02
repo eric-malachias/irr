f8d9891734a3481937fe9a7c3f219d80bdbb52c7
Eric Malachias <malachias.eric@gmail.com>
2018-12-01 23:02:13 -0500

    add test coverage for xirr()

--- tests/irr/xirr.test.ts
+++ tests/irr/xirr.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import sinon from 'sinon'
 import { Polynomial } from '../../src/polynomial/polynomial'
-import { irr } from '../../src'
+import { xirr } from '../../src'
 import { RootFinderOptions, RootFinderMethod } from '../../src/root-finder/definition'
 
 const UNIQUE_ROOT = {
@@ -9,7 +9,15 @@ const UNIQUE_ROOT = {
   iterations: 0,
   value: Math.PI,
 }
-const UNIQUE_COEFFICIENTS = [2, 7, 0, 9]
+const UNIQUE_XIRR_RESULT = {
+  days: 60,
+  rate: Math.PI - 1,
+}
+const UNIQUE_DATA = [
+  { amount: -10, date: '20180101' },
+  { amount: 10, date: '20180201' },
+  { amount: 0.05, date: '20180301' },
+]
 const UNIQUE_OPTIONS: RootFinderOptions = {
   estimate: 'auto',
   epsilon: 1e-5,
@@ -18,15 +26,15 @@ const UNIQUE_OPTIONS: RootFinderOptions = {
   method: RootFinderMethod.Newton,
 }
 
-describe('irr', () => {
+describe('xirr', () => {
   it('uses Polynomial.prototype.findRoot() for the calculation', () => {
     const stub = sinon
       .stub(Polynomial.prototype, 'findRoot')
       .returns(UNIQUE_ROOT)
 
-    const result = irr(UNIQUE_COEFFICIENTS, UNIQUE_OPTIONS)
+    const result = xirr(UNIQUE_DATA, UNIQUE_OPTIONS)
 
-    expect(result).to.equal(UNIQUE_ROOT.value - 1)
+    expect(result).to.deep.equal(UNIQUE_XIRR_RESULT)
     // tslint:disable-next-line no-unused-expression
     expect(stub.calledOnce).to.be.true
 
