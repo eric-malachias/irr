37b10eff913cefa3a73953fba721d08a7e3f5475
Eric Malachias <malachias.eric@gmail.com>
2019-03-03 17:55:01 -0500

    split tests for better issue diagnosing

--- tests/utils/zeros.test.ts
+++ tests/utils/zeros.test.ts
@@ -2,13 +2,15 @@ import { expect } from 'chai'
 import { zeros } from '../../src/utils/zeros'
 
 describe('zeros', () => {
-  it('creates arrays with given length', () => {
-    const cases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  describe('creates arrays with given length', () => {
+    const cases = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     cases.forEach(length => {
-      const expected = new Array(length).fill(0)
+      it(`length = ${length}`, () => {
+        const expected = new Array(length).fill(0)
 
-      expect(zeros(length)).to.deep.equal(expected)
+        expect(zeros(length)).to.deep.equal(expected)
+      })
     })
   })
 })
