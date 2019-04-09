1f787fba354004a749e9aac9bff05b0fbf42f948
Eric Malachias <malachias.eric@gmail.com>
2019-04-08 21:59:09 -0400

    implement function convertRate

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './irr/irr'
 export * from './irr/xirr'
+export * from './irr/convert-rate'
 export { XirrInput } from './irr/definition'
 export { Root, RootFinderMethod, RootFinderOptions } from './root-finder/definition'
---
+++ src/irr/convert-rate.ts
@@ -0,0 +1,29 @@
+export enum RateInterval {
+  Day = 'dai',
+  Week = 'week',
+  Month = 'month',
+  Year = 'year',
+}
+
+function parseInterval (interval: RateInterval | number): number {
+  switch (interval) {
+    case RateInterval.Day:
+      return 1
+    case RateInterval.Week:
+      return 7
+    case RateInterval.Month:
+      return 30
+    case RateInterval.Year:
+      return 365
+  }
+
+  return interval
+}
+
+export function convertRate (
+  rate: number,
+  toInterval: RateInterval | number,
+  fromInterval: RateInterval | number = 1,
+): number {
+  return (1 + rate) ** (parseInterval(toInterval) / parseInterval(fromInterval)) - 1
+}
